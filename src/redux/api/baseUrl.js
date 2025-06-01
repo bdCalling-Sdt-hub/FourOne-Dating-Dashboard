e3a1b0c55fc6f94e078adf3656420f2565c794c4
nimurr <nimurnerob404@gmail.com>
2025-06-01 12:32:09 +0600

    update

--- src/redux/api/baseUrl.js
+++ src/redux/api/baseUrl.js
@@ -1,5 +1,5 @@
 
 // const url = "http://192.168.10.168:5055"
-// const url = "http://192.168.10.168:5050"
-const url = "http://10.0.60.18:8282"
+const url = "https://shadat8282.sobhoy.com"
+// const url = "http://10.0.60.18:8282"
 export default url
